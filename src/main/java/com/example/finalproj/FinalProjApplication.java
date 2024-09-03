53d7c6fa9b3a2c0f2ca80808e2c055637b9f75a4
eunseongjo <tkakfkekdk4@gmail.com>
2024-09-03 13:41:04 +0900

    allmerge commit

--- src/main/java/com/example/finalproj/FinalProjApplication.java
+++ src/main/java/com/example/finalproj/FinalProjApplication.java
@@ -1,9 +1,12 @@
 package com.example.finalproj;
 
+import org.apache.ibatis.annotations.Mapper;
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan(basePackages = "com.example", annotationClass = Mapper.class)
 public class FinalProjApplication {
 
     public static void main(String[] args) {
