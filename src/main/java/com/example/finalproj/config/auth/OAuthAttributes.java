d102080e79b416ab8d9768083f6e5bc9b239807c
eunseongjo <tkakfkekdk4@gmail.com>
2024-09-26 20:16:41 +0900

    feat : token 처리중

--- src/main/java/com/example/finalproj/config/auth/OAuthAttributes.java
+++ src/main/java/com/example/finalproj/config/auth/OAuthAttributes.java
@@ -1,65 +1,65 @@
-//package com.example.finalproj.config.auth;
-//
-//import com.example.finalproj.user.entity.User;
-//import java.util.Map;
-//
-//public class OAuthAttributes {
-//
-//    private Map<String, Object> attributes;
-//    private String nameAttributeKey;
-//    private String name;
-//    private String email;
-//
-//    public OAuthAttributes(Map<String, Object> attributes,
-//                           String nameAttributeKey, String name,
-//                           String email) {
-//        this.attributes = attributes;
-//        this.nameAttributeKey = nameAttributeKey;
-//        this.name = name;
-//        this.email = email;
-//    }
-//
-//    public Map<String, Object> getAttributes() {
-//        return attributes;
-//    }
-//
-//    public void setAttributes(Map<String, Object> attributes) {
-//        this.attributes = attributes;
-//    }
-//
-//    public String getNameAttributeKey() {
-//        return nameAttributeKey;
-//    }
-//
-//    public String getName() {
-//        return name;
-//    }
-//
-//    public String getEmail() {
-//        return email;
-//    }
-//
-//    // OAuth2User에서 반환하는 사용자 정보는 Map
-//    // 따라서 값 하나하나를 변환해야 한다.
-//    public static OAuthAttributes of(String registrationId,
-//                                     String userNameAttributeName,
-//                                     Map<String, Object> attributes) {
-//        return ofGoogle(userNameAttributeName, attributes);
-//    }
-//
-//    // 구글 생성자
-//    private static OAuthAttributes ofGoogle(String userNameAttributeName,
-//                                            Map<String, Object> attributes) {
-//        return new OAuthAttributes(
-//                attributes,
-//                userNameAttributeName,
-//                (String) attributes.get("name"),
-//                (String) attributes.get("email")
-//        );
-//    }
-//
-//    // User 엔티티 생성
-//    public User toEntity() {
-//        return new User(name, email);
-//    }
-//}
+package com.example.finalproj.config.auth;
+
+import com.example.finalproj.user.entity.User;
+import java.util.Map;
+
+public class OAuthAttributes {
+
+    private Map<String, Object> attributes;
+    private String nameAttributeKey;
+    private String name;
+    private String email;
+
+    public OAuthAttributes(Map<String, Object> attributes,
+                           String nameAttributeKey, String name,
+                           String email) {
+        this.attributes = attributes;
+        this.nameAttributeKey = nameAttributeKey;
+        this.name = name;
+        this.email = email;
+    }
+
+    public Map<String, Object> getAttributes() {
+        return attributes;
+    }
+
+    public void setAttributes(Map<String, Object> attributes) {
+        this.attributes = attributes;
+    }
+
+    public String getNameAttributeKey() {
+        return nameAttributeKey;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    // OAuth2User에서 반환하는 사용자 정보는 Map
+    // 따라서 값 하나하나를 변환해야 한다.
+    public static OAuthAttributes of(String registrationId,
+                                     String userNameAttributeName,
+                                     Map<String, Object> attributes) {
+        return ofGoogle(userNameAttributeName, attributes);
+    }
+
+    // 구글 생성자
+    private static OAuthAttributes ofGoogle(String userNameAttributeName,
+                                            Map<String, Object> attributes) {
+        return new OAuthAttributes(
+                attributes,
+                userNameAttributeName,
+                (String) attributes.get("name"),
+                (String) attributes.get("email")
+        );
+    }
+
+    // User 엔티티 생성
+    public User toEntity() {
+        return new User(name, email);
+    }
+}
